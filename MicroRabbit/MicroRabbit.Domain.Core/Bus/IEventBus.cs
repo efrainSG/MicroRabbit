388adcc1588dc86bf4091756cd811c935e366e0f
Efra <efrain_serna@hotmail.com>
2022-04-04 13:58:01 -0500

    Implementation of Interfaces and classes for domain.

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,31 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<CustomType>(CustomType command) where CustomType : Command;
+
+        void Publish<CustomType>(CustomType @event) where CustomType : Event;
+
+        void Subscribe<CustomType, CustomHandler>()
+            where CustomType : Event
+            where CustomHandler : IEventHandler<CustomType>;
+    }
+
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent: Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message:IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
